323a19cb12664933f9df631e42721b56dabdbc16
Carles Vicedo <carlesvicedo@gmail.com>
2021-03-08 17:33:45 +0100

    Header component

--- FRONTEND/city-records/src/app/app.module.ts
+++ FRONTEND/city-records/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { MapComponent } from './map/map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    MapComponent
   ],
   imports: [
     BrowserModule,
