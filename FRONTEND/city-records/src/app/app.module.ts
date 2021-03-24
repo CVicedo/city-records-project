e2977c74c0b069a3da2fdad16ade9c784e565a60
Carles Vicedo <carlesvicedo@gmail.com>
2021-03-24 16:44:44 +0100

    Checking environment

--- FRONTEND/city-records/src/app/app.module.ts
+++ FRONTEND/city-records/src/app/app.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
-import { AgmCoreModule } from '@agm/core'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 import { HttpClientModule } from '@angular/common/http'
 import { MatTableModule } from '@angular/material/table'
-import { environment } from '../environments/environment'
 import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from './app.component'
 import { HeaderComponent } from './components/header/header.component'
@@ -40,9 +38,6 @@ import { NotFoundComponent } from './components/not-found/not-found.component'
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    AgmCoreModule.forRoot({
-      apiKey: environment.API_MAPS_KEY
-    }),
     HttpClientModule,
     HomeModule,
     StoresDashboardModule,
